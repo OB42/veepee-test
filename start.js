642dba3b3e4653f4fee53e5c0262f77a112ad6d7
nooo <john@doe>
2019-08-18 11:29:05 +0200

    add popularity increment

--- start.js
+++ start.js
@@ -1,12 +1,32 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
-
+var data = {};
+var tmp = require(`${__dirname}/${process.env.NAMES_FILE || "names.json"}`);
+for (name in tmp)
+{
+	data[name.toLowerCase()] = {name, times: tmp[name]}
+}
 app.use(bodyParser.json())
 .post('/typeahead/set', function (req, res) {
+	if (req.body && typeof req.body.name !== "undefined"
+	&& typeof data[req.body.name.toLowerCase()] !== "undefined")
+	{
+		data[req.body.name.toLowerCase()].times++;
+		res.status(200);
+	}
+	else {
+		res.status(400)
+	}
 	res.end();
 })
 .get('/typeahead/:prefix', function (req, res) {
-	res.end();
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200);
+	res.end('[]');
+})
+.get('/typeahead/', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.status(200);
+	res.end('[]');
 });
-
 app.listen(process.env.PORT || 8080);
