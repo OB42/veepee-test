a880f3a06c3e1f1febe49cd073e1782087212d16
nooo <john@doe>
2019-08-20 18:15:14 +0200

    Commenting and refactoring

--- start.js
+++ start.js
@@ -1,38 +1,38 @@
 const app = require('express')();
-module.exports = app;
+module.exports = app;//exporting the express server for unit tests
 const bodyParser = require('body-parser');
 const Trie = require('./trie.js');
+
+//turning the data into a prefix tree(trie)
+var tmp = require(`${__dirname}/${process.env.NAMES_FILE || "names.json"}`);
 var tree = new Trie();
 var data = {};
-var tmp = require(`${__dirname}/${process.env.NAMES_FILE || "names.json"}`);
 for (name in tmp)
 {
 	data[name.toLowerCase()] = {name, times: tmp[name]}
 	tree.insert(name, data[name.toLowerCase()]);
 }
 
-app.use(bodyParser.json())
+app
+.use(bodyParser.json())
 .post('/typeahead/set', function (req, res) {
-	if (req.body && typeof req.body.name !== "undefined"
-	&& typeof data[req.body.name.toLowerCase()] !== "undefined")
+	//checking for bad requests
+	if (!req.body || typeof req.body.name === "undefined"
+	|| typeof data[req.body.name.toLowerCase()] === "undefined")
 	{
+		res.status(400)
+		res.end('')
+	}
+	else {
 		data[req.body.name.toLowerCase()].times++;
 		res.status(200);
 		res.end(JSON.stringify(data[req.body.name.toLowerCase()]));
 	}
-	else {
-		res.status(400)
-		res.end('')
-	}
 })
-.get('/typeahead/:prefix', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-	res.status(200);
-	res.end(JSON.stringify(tree.find(req.params.prefix)));
-})
-.get('/typeahead/', function (req, res) {
+.get(/\/typeahead\/(.*)/, function (req, res) {
+	var prefix = req.params[0]
 	res.setHeader('Content-Type', 'application/json');
 	res.status(200);
-	res.end(JSON.stringify(tree.find('')));
-});
+	res.end(JSON.stringify(tree.find(prefix)));
+})
 app.listen(process.env.PORT || 8080);
--- trie.js
+++ trie.js
@@ -1,40 +1,51 @@
 module.exports = class trieNode {
-  constructor(value) {
-	this.children = {};
-	this.value = value;
-  }
-  find(key) {
-	key = key.toLowerCase();
-	var tmp = this;
-	for (var i = 0; i < key.length; i++)
+	constructor(value) {
+		this.children = {};
+		this.value = value;
+	}
+	find(key) {
+		key = key.toLowerCase();
+		var tmp = this;
+		for (var i = 0; i < key.length; i++)
+		{
+				if (tmp.children[key[i]])
+				{
+					tmp = tmp.children[key[i]]
+				}
+				else
+				{
+					return ([]);
+				}
+		}
+		var arr = [];
+		(function recursive_search(node) {
+			if (node.value && node.value.name.toLowerCase() != key)
+			{
+				arr.push(node.value)
+			}
+			for (var i in node.children)
+			{
+				recursive_search(node.children[i])
+			}
+		})(tmp);
+		arr.sort((a, b) => b.times - a.times);
+		if (tmp.value)
+		{
+			arr.unshift(tmp.value);
+		}
+		return (arr.slice(0, process.env.SUGGESTION_NUMBER));
+	}
+	insert(key, value)
 	{
-  		if (tmp.children[key[i]])
-			tmp = tmp.children[key[i]]
-  		else
-			return ([]);
-  	}
-	var arr = [];
-	(function recursive(node)
-	{
-		if (node.value && node.value.name.toLowerCase() != key)
-			arr.push(node.value)
-		for (var i in node.children)
-			recursive(node.children[i])
-	})(tmp);
-	arr.sort((a, b) => b.times - a.times);
-	if (tmp.value)
-		arr.unshift(tmp.value)
-  	return (arr.slice(0, process.env.SUGGESTION_NUMBER));
-  }
-  insert(key, value)
-  {
-	  key = key.toLowerCase();
-	  var tmp = this;
-	  key.split('').forEach(char => {
-	 	 if (!(tmp.children[char]))
-	 		 tmp.children[char] = new trieNode();
-	 	 tmp = tmp.children[char]
-	  })
-	  tmp.value = value;
-  }
+		key = key.toLowerCase();
+		var tmp = this;
+		key.split('').forEach(char => {
+			if (!(tmp.children[char]))
+			{
+				tmp.children[char] = new trieNode();
+			}
+			tmp = tmp.children[char];
+		});
+		tmp.value = value;
+	}
 }
