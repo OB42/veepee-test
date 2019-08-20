e7cca293130bae1967b774e1dfd076e7a1ea28a7
nooo <john@doe>
2019-08-20 17:42:57 +0200

    Add search

--- trie.js
+++ trie.js
@@ -4,17 +4,31 @@ module.exports = class trieNode {
 	this.value = value;
   }
   find(key) {
+	key = key.toLowerCase();
 	var tmp = this;
-  	key.split('').forEach(char => {
-  		if (tmp.children[char])
-  			tmp = tmp.children[char]
+	for (var i = 0; i < key.length; i++)
+	{
+  		if (tmp.children[key[i]])
+			tmp = tmp.children[key[i]]
   		else
-  			return ;
-  	})
-  	return (tmp.value);
+			return ([]);
+  	}
+	var arr = [];
+	(function recursive(node)
+	{
+		if (node.value && node.value.name.toLowerCase() != key)
+			arr.push(node.value)
+		for (var i in node.children)
+			recursive(node.children[i])
+	})(tmp);
+	arr.sort((a, b) => b.times - a.times);
+	if (tmp.value)
+		arr.unshift(tmp.value)
+  	return (arr.slice(0, process.env.SUGGESTION_NUMBER));
   }
   insert(key, value)
   {
+	  key = key.toLowerCase();
 	  var tmp = this;
 	  key.split('').forEach(char => {
 	 	 if (!(tmp.children[char]))
