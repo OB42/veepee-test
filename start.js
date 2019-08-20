cc2f3a5efba84822b1350cb91b5843cc2417e908
nooo <john@doe>
2019-08-20 17:40:54 +0200

    Improving tests

--- start.js
+++ start.js
@@ -1,13 +1,16 @@
 const app = require('express')();
+module.exports = app;
 const bodyParser = require('body-parser');
-var tree = require('./trie.js')
+const Trie = require('./trie.js');
+var tree = new Trie();
 var data = {};
 var tmp = require(`${__dirname}/${process.env.NAMES_FILE || "names.json"}`);
-
 for (name in tmp)
 {
 	data[name.toLowerCase()] = {name, times: tmp[name]}
+	tree.insert(name, data[name.toLowerCase()]);
 }
+
 app.use(bodyParser.json())
 .post('/typeahead/set', function (req, res) {
 	if (req.body && typeof req.body.name !== "undefined"
@@ -15,20 +18,21 @@ app.use(bodyParser.json())
 	{
 		data[req.body.name.toLowerCase()].times++;
 		res.status(200);
+		res.end(JSON.stringify(data[req.body.name.toLowerCase()]));
 	}
 	else {
 		res.status(400)
+		res.end('')
 	}
-	res.end();
 })
 .get('/typeahead/:prefix', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
 	res.status(200);
-	res.end('[]');
+	res.end(JSON.stringify(tree.find(req.params.prefix)));
 })
 .get('/typeahead/', function (req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	res.status(200);
-	res.end('[]');
+	res.end(JSON.stringify(tree.find('')));
 });
 app.listen(process.env.PORT || 8080);
--- test.js
+++ test.js
@@ -1,85 +1,94 @@
 var supertest = require("supertest");
 var should = require("should");
-var server = supertest.agent("localhost:" + (process.env.PORT || 8080));
+var server = supertest(require('./start.js'))
 describe("GET /typeahead/:prefix tests",function(){
-  it(`should return an empty array`, function(done){
-	server
-	.get('/typeahead/abc')
-	.expect("Content-type",/json/)
-	.expect(200)
-	.end(function(err,res){
-	  res.status.should.equal(200);
-	  res.body.should.be.instanceof(Array).and.have.lengthOf(0);
-	  done();
+	describe("No match test",function(){
+		it(`should return an empty array`, function(done){
+		server
+		.get('/typeahead/abc')
+		.expect("Content-type",/json/)
+		.expect(200)
+		.end(function(err,res){
+		  res.status.should.equal(200);
+		  res.body.should.be.instanceof(Array).and.have.lengthOf(0);
+		  done();
+		});
+	  });
+	  });
+	describe("One letter test",function(){
+		it(`should return: [{"name": "James": "times": 43}, {"name": "John": "times": 21}, {"name": "Joanna", "times": 13}, {"name": "Ja", "times": 3}]`, function(done){
+		server
+		.get('/typeahead/j')
+		.expect("Content-type",/json/)
+		.expect(200)
+		.end(function(err,res){
+			res.status.should.equal(200);
+		  	JSON.stringify(res.body).should.equal(JSON.stringify(JSON.parse('[{"name": "James", "times": 43}, {"name": "John", "times": 21}, {"name": "Joanna", "times": 13}, {"name": "Ja", "times": 3}]')));
+		  	done();
+		});
+	  });
+	  });
+	describe("Two letter test",function(){
+		it(`should return: [{"name": "Ja", "times": 3}, {"name": "James", "times": 43}]`, function(done){
+	  server
+	  .get('/typeahead/ja')
+	  .expect("Content-type",/json/)
+	  .expect(200)
+	  .end(function(err,res){
+		  res.status.should.equal(200);
+		  JSON.stringify(res.body).should.equal(JSON.stringify(JSON.parse('[{"name": "Ja", "times": 3}, {"name": "James", "times": 43}]')));
+		  done();
+	  });
+	  });
+	})
+	describe("increasing \"John\" popularity",function(){
+		it(`should return status 200`, function(done){
+		server
+		.post('/typeahead/set')
+		.send({name: "John"})
+		.expect(200)
+		.end(function(err,res){
+		  res.status.should.equal(200);
+		  done();
+		});
+	  });
+	  });
+	describe("no name test",function(){
+		it("should return status 400", function(done){
+	    server
+	    .post('/typeahead/set')
+	    .send({})
+	    .expect(400)
+	    .end(function(err,res){
+	      res.status.should.equal(400);
+	      done();
+	    });
+	  });
+	  });
+	describe("empty json test",function(){
+		it("should return status 400", function(done){
+	    server
+	    .post('/typeahead/set')
+	    .send('')
+	    .expect(400)
+	    .end(function(err,res){
+	      res.status.should.equal(400);
+	      done();
+	    });
+	  });
+	  });
+	describe("empty name test",function(){
+		it("should return status 400", function(done){
+	    server
+	    .post('/typeahead/set')
+	    .send({name: ''})
+	    .expect("Content-type",/json/)
+	    .expect(400)
+	    .end(function(err,res){
+	      res.status.should.equal(400);
+	      done();
+	    });
+	  });
 	});
-  });
-  it(`should return: [{"name": "James": "times": 43}, {"name": "John": "times": 21}, {"name": "Joanna", "times": 13}, {"name": "Ja", "times": 3}]`, function(done){
-	server
-	.get('/typeahead/j')
-	.expect("Content-type",/json/)
-	.expect(200)
-	.end(function(err,res){
-		res.status.should.equal(200);
-	  	res.body.toString().should.equal('[{"name": "James": "times": 43}, {"name": "John": "times": 21}, {"name": "Joanna", "times": 13}, {"name": "Ja", "times": 3}]');
-	  	done();
-	});
-  });
-  it(`should return: [{"name": "Ja", "times": 3}, {"name": "James", "times": 43}]`, function(done){
-  server
-  .get('/typeahead/j')
-  .expect("Content-type",/json/)
-  .expect(200)
-  .end(function(err,res){
-	  res.status.should.equal(200);
-	  res.body.toString().should.equal('[{"name": "James": "times": 43}, {"name": "John": "times": 21}, {"name": "Joanna", "times": 13}, {"name": "Ja", "times": 3}]');
-	  done();
-  });
-  });
-})
 
-describe("POST /typeahead/set tests",function(){
-  it(`should return status 200`, function(done){
-	server
-	.post('/typeahead/set')
-	.send({name: "John"})
-	.expect(200)
-	.end(function(err,res){
-	  res.status.should.equal(200);
-	  done();
-	});
-  });
-  //empty object
-  it("should return status 400", function(done){
-    server
-    .post('/typeahead/set')
-    .send({})
-    .expect(400)
-    .end(function(err,res){
-      res.status.should.equal(400);
-      done();
-    });
-  });
-  //no json
-  it("should return status 400", function(done){
-    server
-    .post('/typeahead/set')
-    .send('')
-    .expect(400)
-    .end(function(err,res){
-      res.status.should.equal(400);
-      done();
-    });
-  });
-  //empty name
-  it("should return status 400", function(done){
-    server
-    .post('/typeahead/set')
-    .send({name: ''})
-    .expect("Content-type",/json/)
-    .expect(400)
-    .end(function(err,res){
-      res.status.should.equal(400);
-      done();
-    });
-  });
 });
